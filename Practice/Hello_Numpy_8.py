ac9d6c26f2e970dc3c8554f98cdbf42b64148961
wuguangkai211 <wuguangkai2018@outlook.com>
2021-01-19 17:33:40 +0800

    change dir

--- Practice/Hello_Numpy_8.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-m, n = 2, 3
-
-X = np.random.rand(m, n)
-Y = X.T                     # 求转置矩阵
-
-print(X)
-print(Y)
--- Practice/Hello_Numpy_9.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-m = 3
-
-X = np.random.rand(m, m)
-Y = np.linalg.inv(X)            # 求方阵的逆矩阵
-
-print(X)
-print(Y)
