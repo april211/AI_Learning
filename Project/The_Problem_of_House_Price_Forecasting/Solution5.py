caa5d94a1b45f3dfd602259d2872d7a9ae911a0a
wuguangkai211 <wuguangkai2018@outlook.com>
2021-05-12 17:42:14 +0800

    r2与模型参数的关系可视化

--- Project/The_Problem_of_House_Price_Forecasting/Solution5.py
+++ Project/The_Problem_of_House_Price_Forecasting/Solution5.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
-import Linear_Regression_SGD_Class as lrsgd
+import Linear_Regression_GD_Class as lrgd
 import matplotlib.pyplot as plt
 
 
@@ -29,35 +29,35 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 X_train = process_features(X_train)
 X_test = process_features(X_test)
 
-eta1 = 1
-N = 700
-eta1_list = np.empty(shape=(1, 0))
+eta = 0.01
+N = 100
+eta_list = np.empty(shape=(1, 0))
 r2_list = np.empty(shape=(1, 0))
 N_list = np.empty(shape=(1, 0))
 
-# 固定 N = 8000，测试 eta_1
-for i in range(70):
-    model1 = lrsgd.LinearRegressionSGD()
-    model1.fit(X_train, y_train, eta1, 8000)
+# 固定 N = 6000，测试 eta
+for i in range(30):
+    model1 = lrgd.LinearRegressionGD()
+    model1.fit(X_train, y_train, eta, 6000)
     y_pred = model1.predict(X_test)
     mse = mean_squared_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
-    eta1_list = np.append(eta1_list, eta1)
+    eta_list = np.append(eta_list, eta)
     r2_list = np.append(r2_list, r2)
     # print("mse = {}, r2 = {}.".format(mse, r2))
-    print(eta1)
-    eta1 += 4
+    print(eta)
+    eta += 0.005
 
 fig, axs = plt.subplots(1, 2, figsize=(10, 4))
-axs[0].set_title('r2 ~ eta_1', size=20)
-axs[0].plot(eta1_list, r2_list, c='b', linewidth=2)
+axs[0].set_title('r2 ~ eta', size=20)
+axs[0].plot(eta_list, r2_list, c='b', linewidth=2)
 
 r2_list = np.empty(shape=(1, 0))
 
-# 固定 eta = 50，测试 N
-for i in range(40):
-    model2 = lrsgd.LinearRegressionSGD()
-    model2.fit(X_train, y_train, 140, N)
+# 固定 eta = 0.14，测试 N
+for i in range(20):
+    model2 = lrgd.LinearRegressionGD()
+    model2.fit(X_train, y_train, 0.14, N)
     y_pred = model2.predict(X_test)
     mse = mean_squared_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
