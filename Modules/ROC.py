734f239c612ed1d3816e64d577d646166dd2e51b
wuguangkai211 <wuguangkai2018@outlook.com>
2021-06-02 17:31:56 +0800

    丰富、优化绘图函数

--- Modules/ROC.py
+++ Modules/ROC.py
@@ -9,14 +9,20 @@ def threshold(t, proba):
 
 def plot_roc_curve(proba, y):
     """ 绘制ROC曲线 """
-    fpr, tpr = [], []
-    for i in range(100):
-        z = threshold(0.01* i, proba)
+    iter_cnt = 400
+    fpr = np.array([])
+    tpr = np.array([]) 
+    for i in range(iter_cnt):
+        z = threshold((1.0 / iter_cnt)* i, proba)
         tp = (y* z).sum()
         fp = ((1 - y)* z).sum()
         tn = ((1 - y)* (1 - z)).sum()
         fn = (y* (1 - z)).sum()
-        fpr.append(1.0* (fp / (fp + tn)))
-        tpr.append(1.0* (tp / (tp + fn)))
+        fpr = np.append(fpr, 1.0* (fp / (fp + tn)))
+        tpr = np.append(tpr, 1.0* (tp / (tp + fn)))
+    plt.title("ROC")
+    plt.xlabel("FPR")
+    plt.ylabel("TPR")
     plt.plot(fpr, tpr)
     plt.show()
+    return fpr, tpr
