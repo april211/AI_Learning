a1fef46ff816f7d64c0dd1e6013c3e562bf825a2
wuguangkai211 <wuguangkai2018@outlook.com>
2021-06-05 21:59:10 +0800

    加入更多参数的测试

--- Project/Ink_Blot_Classification_Problem/Explore.py
+++ Project/Ink_Blot_Classification_Problem/Explore.py
@@ -3,23 +3,22 @@ import numpy as np
 sys.path.append(os.getcwd() + r'\Modules')
 from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
-from Perceptron_Class import Perceptron
 
 # 样本数目、聚类中心数目、特征组维度、每个类别的方差（数组形式）、随机模式
-X, y = make_blobs(n_samples=100, centers=2, n_features=2, cluster_std=0.6, random_state=0)
+X, y = make_blobs(n_samples=100, centers=3, n_features=2, cluster_std=0.2, random_state=10, center_box=(-10.0, 10.0))
 
-for i in range(0, 100):
-    if y[i] == 0:
-        y[i] = -1
 
-print(X)
-print(y)
+""" print(X)
+print(y) """
 
 # X 有两列，每一行：(x1, x2)，作为数据点；y是标签值
-# plt.plot(X[:, 0][y==1], X[:, 1][y==1], "bs", ms=3)          # 以蓝色实心方块显示
-# plt.plot(X[:, 0][y==0], X[:, 1][y==0], "yo", ms=3)          # 以黄色实心圆表示，ms表示点的大小
+plt.scatter(X[:, 0][y==0], X[:, 1][y==0], c='r', s=5)
+plt.scatter(X[:, 0][y==1], X[:, 1][y==1], c='b', s=5)
+plt.scatter(X[:, 0][y==2], X[:, 1][y==2], c='y', s=5)
+""" plt.scatter(X[:, 0][y==3], X[:, 1][y==3], c='purple', s=5)
+plt.scatter(X[:, 0][y==4], X[:, 1][y==4], c='orange', s=5) """
 
-# plt.show()
+plt.show()
 
 
 # https://www.jianshu.com/p/891f46e0125e
